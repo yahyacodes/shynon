7c8a4822160b6605920fbcae6c155e06fd53e3c2
yahyacodes <yahya.codin@gmail.com>
2022-05-31 23:32:58 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoieWFoeWFjb2RlcyIsImEiOiJjbDN1bGNnZ2cwMWE5M3BxbnM0ZTNxMmdoIn0.NLEsGo_HUnzlkgfLY08VdQ";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.060982, 42.35725],
+  zomm: 18,
+});
